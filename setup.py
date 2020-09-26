bc8ec0d51e0f5e944defeb5dfaf5832839308959
Oliver Yeun <13575302+oyeun@users.noreply.github.com>
2020-09-25 23:02:22 -0700

    update setup.py and add MANIFEST.in

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@
 from distutils.core import setup, Extension
 
 SPEEX_SRC_DIR = "pySHSpeex/soundhound-speex"
-SOURCES = "cb_search.c exc_10_32_table.c exc_8_128_table.c filters.c gain_table.c hexc_table.c high_lsp_tables.c lsp.c ltp.c speex.c stereo.c vbr.c vq.c bits.c exc_10_16_table.c exc_20_32_table.c exc_5_256_table.c exc_5_64_table.c gain_table_lbr.c hexc_10_32_table.c lpc.c lsp_tables_nb.c modes.c modes_wb.c nb_celp.c quant_lsp.c sb_celp.c speex_callbacks.c speex_header.c window.c soundhound.c"
+SOURCES = "cb_search.c exc_10_32_table.c exc_8_128_table.c filters.c gain_table.c hexc_table.c high_lsp_tables.c " \
+          "lsp.c ltp.c speex.c stereo.c vbr.c vq.c bits.c exc_10_16_table.c exc_20_32_table.c exc_5_256_table.c " \
+          "exc_5_64_table.c gain_table_lbr.c hexc_10_32_table.c lpc.c lsp_tables_nb.c modes.c modes_wb.c nb_celp.c " \
+          "quant_lsp.c sb_celp.c speex_callbacks.c speex_header.c window.c soundhound.c "
 SOURCES = [SPEEX_SRC_DIR + "/src/%s" % x for x in SOURCES.split()]
 
 ext_modules = [Extension('pySHSpeex',
@@ -18,6 +21,8 @@ setup(name='Houndify',
       version='2.0',
       license='MIT',
       description='Houndify libraries and SoundHound speex encoder',
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
       author='Soundhound Inc.',
       maintainer='Oliver Yeun',
       url='https://github.com/oyeun/houndify',
